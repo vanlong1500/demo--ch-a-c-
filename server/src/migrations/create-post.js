ca823f0d2c8eba964961f7e9850afb59229a9372
vanlong1500 <dangvanlong151002@gmail.com>
2023-11-08 20:41:05 +0700

    first commit

---
+++ server/src/migrations/create-post.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Posts', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.STRING
+            },
+            title: {
+                type: Sequelize.STRING
+            },
+            address: {
+                type: Sequelize.STRING
+            },
+            cateloryCode: {
+                type: Sequelize.STRING
+            },
+            userId: {
+                type: Sequelize.STRING
+            },
+            imagesId: {
+                type: Sequelize.STRING
+            },
+            overviewId: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Posts');
+    }
+};
